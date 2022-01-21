5fa8eaa0df3c448fe53a5ed88f634ce0c02449b5
Edoardo Lai <edoardolai96@gmail.com>
2022-01-21 18:04:49 +0100

    Esercizio terminato

--- main.js
+++ main.js
@@ -5,14 +5,18 @@ let myContainer = document.getElementById('mioContenitore');
 for(let i=1; i <= 100; i++){
 
     
-    if( i%3 == 0) {
-        myContainer.innerHTML +=  "<div class='box-multiplo-3'>" + i + "</div>";
+    if( i%5 ==0 && i%3 == 0) {
+        myContainer.innerHTML +=  "<div class='box-multiplo-3-5'>" + "buzzfizz" + "</div>";
         
     } else if (i%5 == 0){
-        myContainer.innerHTML +=  "<div class='box-multiplo-5'>" + i + "</div>";
+        myContainer.innerHTML +=  "<div class='box-multiplo-5'>" + "fizz" + "</div>";
         
-    } else if (i%5 ==0 && i%3 == 0){
-        myContainer.innerHTML +=  "<div class='box-multiplo-3-5'>" + i + "</div>";
+    } else if (i%3 == 0){
+        myContainer.innerHTML +=  "<div class='box-multiplo-3'>" + "buzz" + "</div>";
+        
+    } else {
+        myContainer.innerHTML +=  "<div class='box-normale'>" + i + "</div>";
+
     }
 
 
